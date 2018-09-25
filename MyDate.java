f1d7a06f58efc6b9527395f042a6be2618b55012
PatelTejash76 <patelt@fontbonne.edu>
2018-09-25 18:57:08 -0500

    Get date 2

--- MyDate.java
+++ MyDate.java
@@ -34,7 +34,7 @@ public class MyDate{
 		return my_date2;
 	}
 	
-	/*public String GetMyDateString1() {
+	public String GetMyDateString1() {
 		//please return a date string in the format of mm/dd/yyyy
 		String [] str_array = my_date1.split("/");
 		String my_date2 = String.join("",str_array[0],str_array[1],str_array[2]);
@@ -42,8 +42,12 @@ public class MyDate{
 	}
 	
 	public String GetMyDateString2() {
+            // return null;
 		//please return a date string in the format of dd/mm/yyyy
-	}*/
+               String [] str_array = my_date1.split("/");
+               String my_date = String.join("",str_array[1],"/",str_array[0], "/", str_array[2]);
+		return my_date; 
+	}
 	
 	public boolean IsLeapYear(int year) {
 		//please return true if the year is a leap year
